c670f031ab4f7edae40f022b7667a051652b2074
Iara Pou <iarapou21@hotmail.com>
2022-11-22 14:33:41 -0300

    Acomoda nombres de direcciones

--- ejercicio 4/index.js
+++ ejercicio 4/index.js
@@ -1,14 +1,9 @@
-const http = require("http");
-const colors = require("colors")
+const express = require("express");
+const server = express();
 
-const handleServer = function(req, res){
-    res.writeHead(200, {'Content-type':'text/html'});
-    res.write("<h1 style='color: pink;'>Hola mundo</h1>")
-    res.end();
-    };
-
-const server = http.createServer(handleServer);
 server.listen(3000, function(){
-    console.log("Servidor levantado en puerto 3000".rainbow)
-});    
-
+    console.log("El server está funcionando correctamente en el puerto 3000.")
+})
+server.get("/", function(req, res){
+    res.send("<h1>Hola mundo desde express con Nodejs</h1>")
+})
