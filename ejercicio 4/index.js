fe52601c2ef9202b171131d9ce01e616214b5a93
Iara Pou <iarapou21@hotmail.com>
2022-11-22 14:31:20 -0300

    Mejora el texto

--- ejercicio 4/index.js
+++ ejercicio 4/index.js
@@ -3,12 +3,12 @@ const colors = require("colors")
 
 const handleServer = function(req, res){
     res.writeHead(200, {'Content-type':'text/html'});
-    res.write("<h1 style='color: pink;'>Hola</h1>")
+    res.write("<h1 style='color: pink;'>Hola mundo</h1>")
     res.end();
     };
 
 const server = http.createServer(handleServer);
 server.listen(3000, function(){
-    console.log("servidor levantado en puerto 3000".rainbow)
+    console.log("Servidor levantado en puerto 3000".rainbow)
 });    
 
